187d620464b944395bbfe7cddd0875d88798b4e0
jadit19 <jadit19@gmail.com>
2024-08-30 22:50:51 +0530

    remove use of auto keyword

--- include/nexus/pool.h
+++ include/nexus/pool.h
@@ -28,8 +28,9 @@ public:
   std::future<typename std::invoke_result<F, Args...>::type>
   enqueue(F &&f, Args &&...args) {
     using return_type = typename std::invoke_result<F, Args...>::type;
-    auto task = std::make_shared<std::packaged_task<return_type()>>(
-        std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+    std::shared_ptr<std::packaged_task<return_type()>> task =
+        std::make_shared<std::packaged_task<return_type()>>(
+            std::bind(std::forward<F>(f), std::forward<Args>(args)...));
     std::future<return_type> result = task->get_future();
     {
       std::unique_lock<std::mutex> lock(mutex);
